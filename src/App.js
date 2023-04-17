64b0399bfcd82cfa7e0fa695f0094da2222a4382
avichay kadosh <avichayn@g.jct.ac.il>
2023-04-18 01:11:54 +0300

    i fix that the text area response to the buttons bellow

--- src/App.js
+++ src/App.js
@@ -5,36 +5,59 @@ import TextEditor from './components/textEditor';
 class App extends Component {
   state = {
     text: '',
+    language: 'english',
+    size: 'medium',
+    font: 'Arial',
+    color: 'black',
+    camelCase: false,
   };
+  handleOptionClick = (event) => {
+    const key = event.target.id;
+    const text = this.state.text;
+    const camelCase = this.state.camelCase;
+    const language = this.state.language;
+    if (value === '16px') {
+      this.setState({ text: text });
+    }
+  };
+  handleKeyClick = (event) => {
+    const key = event.target.id;
+    const text = this.state.text;
+    const camelCase = this.state.camelCase;
+    const language = this.state.language;
+
+    if (key === 'Enter') {
+      this.setState({ text: text + '\n' });
+    } else if (key === 'Backspace') {
+      this.setState({ text: text.slice(0, -1) });
+    } else if (key === 'Tab') {
+      this.setState({ text: text + '    ' });
+    } else if (key === 'Shift') {
+      this.setState({ camelCase: !camelCase });
+    } else if (key === 'Control') {
+      this.setState({ language: 'english' });
+    } else if (key === 'Clear') {
+      this.setState({ text: '' });
+    } else if (language === 'english') {
+      this.setState({ text: text + key });
+    }
+  };
+
   render() {
     return (
       <div>
-        <TextEditor>
-          TextEditor={this.props.text}
-          onKeyClick={this.handleKeyClick}
-          {/* handleTextChange={this.handleTextChange} */}
-        </TextEditor>
+        <TextEditor
+          value={this.state.text}
+          handleKeyPress={this.handleKeyClick}
+          language={this.state.language}
+          size={this.state.size}
+          font={this.state.font}
+          color={this.state.color}
+          camelCase={this.state.camelCase}
+        />
       </div>
     );
   }
-  // handleTextChange = (event) => {
-  //   this.setState({ text: event.target.value });
-  // };
-
-  handleKeyClick = (event) => {
-    console.log(event.target);
-    // if (key === 'backspace') {
-    //   this.setState((prevState) => ({
-    //     text: prevState.text.slice(0, -1),
-    //   }));
-    // } else if (key === 'clear') {
-    //   this.setState({ text: '' });
-    // } else {
-    //   this.setState((prevState) => ({
-    //     text: prevState.text + key,
-    //   }));
-    // }
-  };
 }
 
 export default App;
@@ -43,5 +66,5 @@ export default App;
 // function App() {
 //   return (
 //   );
-// }
+// }
 //TextEditor}
